41c3be2f1c3db053cf08048c1ef6b22834373a0d
ArthurLapaev <arthurlapaev@coherentsolutions.com>
2020-03-03 18:28:13 +0300

    add backgrounds add styles

--- src/resources/elements/home/weather-main/weather-main.js
+++ src/resources/elements/home/weather-main/weather-main.js
@@ -25,13 +25,39 @@ export class MarketMain {
     }
 
     _addCity(city) {
+      const icon = this.weatherApi.getIconUrl(city.weather[0].icon);
+      const mode = this.weatherApi.getModeDay(city.weather[0].icon);
       this.cities.push({
         name: city.name,
         weather: city.weather[0].main,
         temperature: city.main.temp > 0 ? `+${Math.round(city.main.temp)}` : Math.round(city.main.temp),
-        icon: this.weatherApi.getIconUrl(city.weather[0].icon),
-        mode: this.weatherApi.getModeDay(city.weather[0].icon)
+        icon: icon,
+        mode: mode,
+        class: this._getCssClass(mode, city.weather[0].main)
       });
+    }
+
+    _getCssClass(mode, weather) {
+      let cssClass = '';
+      switch (weather) {
+      case 'Clouds':
+        cssClass = `weather-cloud-${mode}`;
+        break;
+      case 'Clear':
+        cssClass = `weather-clear-${mode}`;
+        break;
+      case 'Rain':
+        cssClass = `weather-rain-${mode}`;
+        break;
+      case 'Fog':
+      case 'Mist':
+        cssClass = `weather-fog-${mode}`;
+        break;
+      default:
+        break;
+      }
+
+      return cssClass;
     }
 }
 
